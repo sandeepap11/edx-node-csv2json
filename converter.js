4f232a3fb0c6317464d59240222bf617240b359c
sandeepramachandra <sandeepap11@gmail.com>
2018-03-08 23:16:56 +0530

    doc: add comments

--- converter.js
+++ converter.js
@@ -1,33 +1,49 @@
 module.exports = (inputfile = 'customer-data.csv') => {
 
+// Import fs, path and os modules
   const fs = require('fs');
   const path = require('path');
   const os = require('os');
 
+/**
+ * Converts line of CSV text to Javascript object
+ * @param {string} header The header from csv.
+ * @param {string} line A row from the csv.
+ * @return {object} The resultant object with header as key and corresponding value from csv as value.
+**/
   const convertToJSON = (header, line) => {
 
     let result = {};
 
+    // Get keys and values via splitting
     const keys = header.split(',');
     const values = line.split(',');
 
+    // Assign values to corresponding keys
     for(let i = 0; i < keys.length; i++)
     result[keys[i]] = values[i];
 
     return result;
 
   }
-
+  /**
+   * Gets data from file read operationof csv and converts to JSON file.
+   * @param {string} data Data read from the csv file.
+  **/
   const handleData = (data) => {
 
     const outputFile = 'customer-data.json';
     const endOfLine = os.EOL;
+
+    // Convert file data into separate lines by splitting using end of line character (/n, /r/n, etc., based on OS).
     const allLines = data.split(endOfLine);
     let jsonValue = [];
 
+    // Call the convertToJSON method on each line and push the resultant object to array.
     for(let i = 1; i < allLines.length; i ++)
       jsonValue.push(convertToJSON(allLines[0], allLines[i]));
 
+      // Write the JSON object into a file
       fs.writeFile(path.join(__dirname, outputFile), JSON.stringify(jsonValue), 'utf8', (error) => {
         if(error) console.error(error);
         else {
@@ -37,6 +53,7 @@ module.exports = (inputfile = 'customer-data.csv') => {
       });
   }
 
+  // Read the input CSV file and use the handleData function to write to JSON file.
   fs.readFile(path.join(__dirname, inputfile), {encoding: 'utf-8'}, (error, data) => {
 
     if(error) return console.error(error);
--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 const converter = require('./converter');
 
+/**
+* This is the file that needs to run via node command.
+* Pass a csv file as argument. If not customer-data.csv will be taken by default
+**/
 converter(process.argv[2]);
